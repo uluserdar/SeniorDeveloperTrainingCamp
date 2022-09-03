6ea45caf361c935d81de6908e8e5d731bd2c0897
uluserdar <serdar.uludag@hotmail.com>
2022-09-04 00:08:52 +0300

    BaseDbContext created

---
+++ Kodlama.io.Ders.Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Kodlama.io.Ders.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Ders.Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration;
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguage>(pl =>
+            {
+                pl.ToTable("ProgrammingLanguages");
+                pl.HasKey(k=> k.Id);
+                pl.Property(p => p.Id).HasColumnName("Id");
+                pl.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            ProgrammingLanguage[] programmingLanguageSeeds = { new(1, "C#"), new(2, "Java"), new(3, "Pyton"), new(4, "Php") };
+            modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageSeeds);
+        }
+    }
+}
