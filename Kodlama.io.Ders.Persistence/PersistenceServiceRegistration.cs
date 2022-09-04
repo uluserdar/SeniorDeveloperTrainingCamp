c86b7560d0bc6dbf1df957aa309a0301ee060084
uluserdar <serdar.uludag@hotmail.com>
2022-09-04 21:45:43 +0300

    Code review and Data migration

--- Kodlama.io.Ders.Persistence/PersistenceServiceRegistration.cs
+++ Kodlama.io.Ders.Persistence/PersistenceServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using Kodlama.io.Ders.Application.Services.Repositories;
 using Kodlama.io.Ders.Persistence.Contexts;
+using Kodlama.io.Ders.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,7 +12,7 @@ namespace Kodlama.io.Ders.Persistence
         public static IServiceCollection AddPersistenceService(this IServiceCollection services,IConfiguration configuration)
         {
             services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("KodlamaIoDersConnectionString")));
-            services.AddScoped<IProgrammingLanguageRepository,IProgrammingLanguageRepository>();
+            services.AddScoped<IProgrammingLanguageRepository,ProgrammingLanguageRepository>();
 
             return services;
         }
