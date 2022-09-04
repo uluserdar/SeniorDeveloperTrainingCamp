f5f3d39d4636be999a4fcc18013084198b9aff62
uluserdar <serdar.uludag@hotmail.com>
2022-09-04 22:13:38 +0300

    CreateProgrammingLanguageCommand added

---
+++ Kodlama.io.Ders.Application/Features/ProgrammingLanguages/Dtos/CreateProgrammingLanguageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Kodlama.io.Ders.Application.Features.ProgrammingLanguages.Dtos
+{
+    public class CreateProgrammingLanguageDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
