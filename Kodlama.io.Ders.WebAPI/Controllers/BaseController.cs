1d24adb906af7c75042aea24b2ead54a977a9dc7
uluserdar <serdar.uludag@hotmail.com>
2022-09-04 22:24:51 +0300

    ProgrammingLanguagesController added

--- Kodlama.io.Ders.WebAPI/Controllers/BaseController.cs
+++ Kodlama.io.Ders.WebAPI/Controllers/BaseController.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Kodlama.io.Ders.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
     public class BaseController : ControllerBase
     {
         protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
---
+++ Kodlama.io.Ders.WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -0,0 +1,19 @@
+﻿using Kodlama.io.Ders.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Kodlama.io.Ders.Application.Features.ProgrammingLanguages.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodlama.io.Ders.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguagesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreateProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+    }
+}
