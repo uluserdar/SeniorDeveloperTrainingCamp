28e081d78a47a3362bf8965c27ba60fea3722a22
uluserdar <serdar.uludag@hotmail.com>
2022-09-04 21:19:26 +0300

    Code review and connectionstring name changed in appsetings.json

--- Kodlama.io.Ders.Persistence/Contexts/BaseDbContext.cs
+++ Kodlama.io.Ders.Persistence/Contexts/BaseDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using Kodlama.io.Ders.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kodlama.io.Ders.Persistence.Contexts
 {
