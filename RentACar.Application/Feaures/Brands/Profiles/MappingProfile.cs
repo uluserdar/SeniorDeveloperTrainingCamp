057eaf21abe897f76ea0abf39104ccaa193f3086
uluserdar <serdar.uludag@hotmail.com>
2022-09-05 19:20:39 +0300

    RentACar Project entegrated

---
+++ RentACar.Application/Feaures/Brands/Profiles/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Core.Persistence.Paging;
+using RentACar.Application.Feaures.Brands.Commands.CreateBrand;
+using RentACar.Application.Feaures.Brands.Dtos;
+using RentACar.Application.Feaures.Brands.Models;
+using RentACar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.someFeature.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+            CreateMap<Brand,CreateBrandCommand>().ReverseMap();
+            CreateMap<IPaginate<Brand>, BrandListModel>().ReverseMap();
+            CreateMap<Brand, BrandListDto>().ReverseMap();
+            CreateMap<Brand, BrandGetByIdDto>().ReverseMap();
+        }
+    }
+}
---
+++ RentACar.WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Application.Requests;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Feaures.Brands.Commands.CreateBrand;
+using RentACar.Application.Feaures.Brands.Dtos;
+using RentACar.Application.Feaures.Brands.Models;
+using RentACar.Application.Feaures.Brands.Queries.GetListBrand;
+
+namespace RentACar.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController:BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandDto result = await Mediator.Send(createBrandCommand);
+            return Created("", result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            BrandListModel result = await Mediator.Send(getListBrandQuery);
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdBrandQuery getByIdBrandQuery)
+        {
+            BrandGetByIdDto brandGetByIdDto = await Mediator.Send(getByIdBrandQuery);
+            return Ok(brandGetByIdDto);
+        }
+    }
+}
