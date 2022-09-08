5c7a6312adf424688ac4c6cd6057ed106eccaf51
uluserdar <serdar.uludag@hotmail.com>
2022-09-08 21:48:26 +0300

    Kodlama.io.Devs.Domain\Entites => FrameworkTechnology.cs added

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Domain/Entities/FrameworkTechnology.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Domain.Entities
+{
+    public class FrameworkTechnology:Entity
+    {
+        public ProgrammingLanguage? ProgrammingLanguage { get; set; }
+        public int ProgrammigLanguageId { get; set; }
+        public string Name { get; set; }
+
+        public FrameworkTechnology() { }
+
+        public FrameworkTechnology(int id,int programmigLanguageId, string name):this()
+        {
+            Id = id;
+            ProgrammigLanguageId = programmigLanguageId;
+            Name = name;
+        }
+    }
+}
