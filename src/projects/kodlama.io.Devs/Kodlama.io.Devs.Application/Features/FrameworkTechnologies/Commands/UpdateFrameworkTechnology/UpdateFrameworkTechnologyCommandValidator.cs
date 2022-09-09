deece4102a54f4e5b29fb305c75640fb1c9af9fc
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 16:59:05 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Commands\UpdateFrameworkTechnology => UpdateFrameworkTechnologyCommandValidator.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Commands/UpdateFrameworkTechnology/UpdateFrameworkTechnologyCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.UpdateFrameworkTechnology
+{
+    public class UpdateFrameworkTechnologyCommandValidator:AbstractValidator<UpdateFrameworkTechnologyCommand>
+    {
+        public UpdateFrameworkTechnologyCommandValidator()
+        {
+            RuleFor(x=> x.Id).NotEmpty();
+            RuleFor(x=> x.Id).NotNull();
+            RuleFor(x => x.Id).GreaterThan(0);
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).MaximumLength(20);
+        }
+    }
+}
