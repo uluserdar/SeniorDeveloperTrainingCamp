adbd32da7d0df3e2eab7384875c8dc777a35277c
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 17:55:25 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Commands\DeleteFrameworkTechnology => DeleteFrameworkTechnologyCommandValidator.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Commands/DeleteFrameworkTechnology/DeleteFrameworkTechnologyCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.DeleteFrameworkTechnology
+{
+    public class DeleteFrameworkTechnologyCommandValidator:AbstractValidator<DeleteFrameworkTechnologyCommand>
+    {
+        public DeleteFrameworkTechnologyCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Id).GreaterThan(0);
+        }
+    }
+}
