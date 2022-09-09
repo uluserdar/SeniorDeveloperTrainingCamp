1e976347a7a138f8aaebac23bdd31085df21530c
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 19:39:57 +0300

    Kodlama.io.Devs.WebAPI\Controllers => FrameworkTechnologiesController.cs edited

--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/FrameworkTechnologiesController.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Controllers/FrameworkTechnologiesController.cs
@@ -1,8 +1,13 @@
-﻿using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.CreateFrameworkTechnology;
+﻿using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.CreateFrameworkTechnology;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.DeleteFrameworkTechnology;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.UpdateFrameworkTechnology;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Models;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Queries.GetByIdFrameworkTechnology;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Queries.GetListByDynamicFrameworkTechnology;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Queries.GetListFrameworkTechnology;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -31,6 +36,30 @@ namespace Kodlama.io.Devs.WebAPI.Controllers
         {
             DeletedFrameworkTechnologyDto result = await Mediator.Send(deleteFrameworkTechnologyCommand);
             return Ok(result);
+        }
+
+        [HttpGet("{Id")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdFrameworkTechnologyQuery getByIdFrameworkTechnologyQuery)
+        {
+            FrameworkTechnologyGetByIdDto frameworkTechnologyGetByIdDto = await Mediator.Send(getByIdFrameworkTechnologyQuery);
+            return Ok(frameworkTechnologyGetByIdDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListFrameworkTechnologyQuery getListFrameworkTechnologyQuery = new() { PageRequest = pageRequest };
+            FrameworkTechnologyListModel frameworkTechnologyListModel= await Mediator.Send(getListFrameworkTechnologyQuery);
+            return Ok(frameworkTechnologyListModel);
+        }
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListByDynamicFrameworkTechnologyQuery getListByDynamicFrameworkTechnologyQuery = new() { PageRequest = pageRequest, Dynamic = dynamic };
+
+            FrameworkTechnologyListModel frameworkTechnologyListModel = await Mediator.Send(getListByDynamicFrameworkTechnologyQuery);
+            return Ok(frameworkTechnologyListModel);
         }
 
     }
