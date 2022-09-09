df328cb33e45e70e1fa610cc43e401c18db0d58b
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 12:28:51 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Profiles => MappingProfile created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Profiles
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<FrameworkTechnology, CreatedFrameworkTechnologyDto>().ReverseMap();
+        }
+    }
+}
