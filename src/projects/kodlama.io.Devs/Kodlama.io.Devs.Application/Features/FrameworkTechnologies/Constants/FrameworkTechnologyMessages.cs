c7bb4ef6a9a843a221e8a2bbbe1491a19c0a4486
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 12:58:55 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Rules => FrameworkTechnologyBusinessRule.cs created
    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Constants => FrameworkTechnologyMessages.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Constants/FrameworkTechnologyMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Constants
+{
+    public static class FrameworkTechnologyMessages
+    {
+        public static string FrameworkTechnologyNameExistsMessage => "Framework technology name exists.";
+
+    }
+}
---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Rules/FrameworkTechnologyBusinessRule.cs
@@ -0,0 +1,26 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Constants;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Rules
+{
+    public class FrameworkTechnologyBusinessRule
+    {
+        private readonly IFrameworkTechnologyRepository _frameworkTechnologyRepository;
+
+        public FrameworkTechnologyBusinessRule(IFrameworkTechnologyRepository frameworkTechnologyRepository)
+        {
+            _frameworkTechnologyRepository = frameworkTechnologyRepository;
+        }
+        public async Task FrameworkTechnologyNameCanNotBeDublicatedWhenInserted(string name)
+        {
+            var result=await _frameworkTechnologyRepository.GetListAsync(ft=> ft.Name==name);
+            if (result.Items.Any()) throw new BusinessException(FrameworkTechnologyMessages.FrameworkTechnologyNameExistsMessage);
+        }
+    }
+}
