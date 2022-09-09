ece7f6fdcff8e69457945bc7132cd4427e42071b
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 12:44:38 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Commands\CreateFrameworkTechnology => CreateFrameworkTechnologyCommandValidator.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Commands/CreateFrameworkTechnology/CreateFrameworkTechnologyCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.CreateFrameworkTechnology
+{
+    public class CreateFrameworkTechnologyCommandValidator:AbstractValidator<CreateFrameworkTechnologyCommand>
+    {
+        public CreateFrameworkTechnologyCommandValidator()
+        {
+            RuleFor(x=> x.Name).NotEmpty();
+            RuleFor(x => x.Name).MaximumLength(20);
+        }
+    }
+}
