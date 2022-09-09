cc526103e0c1b8dae34ccd7c5927e322bdb6b04f
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 09:48:50 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Dtos => CreatedFrameworkTechnologyDto.cs created
    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Commands\CreateFrameworkTechnology => CreateFrameworkTechnologyCommand.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Commands/CreateFrameworkTechnology/CreateFrameworkTechnologyCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.CreateFrameworkTechnology
+{
+    public class CreateFrameworkTechnologyCommand : IRequest<CreatedFrameworkTechnologyDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateFrameworkTechnologyCommandHandler : IRequestHandler<CreateFrameworkTechnologyCommand, CreatedFrameworkTechnologyDto>
+        {
+            private readonly IMapper _mapper;
+            private readonly IFrameworkTechnologyRepository _frameworkTechnologyRepository;
+
+            public CreateFrameworkTechnologyCommandHandler(IMapper mapper, IFrameworkTechnologyRepository frameworkTechnologyRepository)
+            {
+                _mapper = mapper;
+                _frameworkTechnologyRepository = frameworkTechnologyRepository;
+            }
+
+            public async Task<CreatedFrameworkTechnologyDto> Handle(CreateFrameworkTechnologyCommand request, CancellationToken cancellationToken)
+            {
+                FrameworkTechnology mappedFrameworkTechnology = _mapper.Map<FrameworkTechnology>(request);
+                FrameworkTechnology createdFrameworkTechnology = await _frameworkTechnologyRepository.AddAsync(mappedFrameworkTechnology);
+                CreatedFrameworkTechnologyDto createdFrameworkTechnologyDto=_mapper.Map<CreatedFrameworkTechnologyDto>(createdFrameworkTechnology);
+                return createdFrameworkTechnologyDto;
+            }
+        }
+    }
+}
