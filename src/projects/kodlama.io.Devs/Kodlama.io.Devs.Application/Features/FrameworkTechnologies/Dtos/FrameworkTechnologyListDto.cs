15e1ab71399b5d6aba5f92ca929436c979f16e6c
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 18:34:00 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Dtos => FrameworkTechnologyListDto.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Dtos/FrameworkTechnologyListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos
+{
+    public class FrameworkTechnologyListDto
+    {
+        public int Id { get; set; }
+        public string FrameworkTechnologyName { get; set; }
+        public string ProgrammingLanguageName { get; set; }
+    }
+}
