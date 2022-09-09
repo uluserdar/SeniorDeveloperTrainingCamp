dec9009fd679f846c32281c9bd65fd36d00aa3bc
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 16:42:12 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Dtos => UpdatedFrameworkTechnologyDto.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Dtos/UpdatedFrameworkTechnologyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos
+{
+    public class UpdatedFrameworkTechnologyDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
