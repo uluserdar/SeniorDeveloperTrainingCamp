caeab82d1ab2d5cd888a71f5780ce8d5d57b3b4a
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 09:26:16 +0300

    Kodlama.io.Devs.Persistence\Repositories => FrameworkTechnologyRepository.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Persistence/Repositories/FrameworkTechnologyRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using Kodlama.io.Devs.Persistence.Contexts;
+
+namespace Kodlama.io.Devs.Persistence.Repositories
+{
+    public class FrameworkTechnologyRepository : EfRepositoryBase<FrameworkTechnology, BaseDbContext>, IFrameworkTechnologyRepository
+    {
+        public FrameworkTechnologyRepository(BaseDbContext context) : base(context) { }
+    }
+}
