2a101333a525b8a19451e57e9644dad3b330af41
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 17:39:29 +0300

    code review
    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Rules => FrameworkTechnologyBusinessRule.cs edited
    
    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Constants =>FrameworkTechnologyMessages.cs edited

--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Constants/FrameworkTechnologyMessages.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Constants/FrameworkTechnologyMessages.cs
@@ -9,6 +9,7 @@ namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Constants
     public static class FrameworkTechnologyMessages
     {
         public static string FrameworkTechnologyNameExistsMessage => "Framework technology name exists.";
+        public static string FrameworkTechnologyShouldExistsWhenRequestMessage => "Programming languge not exists.";
 
     }
 }
--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Rules/FrameworkTechnologyBusinessRule.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Rules/FrameworkTechnologyBusinessRule.cs
@@ -1,14 +1,8 @@
 ﻿using Core.CrossCuttingConcerns.Exceptions;
 using Core.Persistence.Paging;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Constants;
-using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Constans;
 using Kodlama.io.Devs.Application.Services.Repositories;
 using Kodlama.io.Devs.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Rules
 {
@@ -29,13 +23,13 @@ namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Rules
         public async Task FrameworkTechnologyNameCanNotBeDublicatedWhenUpdated(int id, string name)
         {
             IPaginate<FrameworkTechnology> result = await _frameworkTechnologyRepository.GetListAsync(pl => pl.Name == name && pl.Id != id);
-            if (result.Items.Any()) throw new BusinessException(ProgrammingLanguageMessages.ProgrammingLanguageNameExistsMessage);
+            if (result.Items.Any()) throw new BusinessException(FrameworkTechnologyMessages.FrameworkTechnologyNameExistsMessage);
         }
 
         public async Task FrameworkTechnologyShouldExistsWhenRequested(int id)
         {
             var result = await _frameworkTechnologyRepository.GetAsync(pl => pl.Id == id);
-            if (result == null) throw new BusinessException(ProgrammingLanguageMessages.ProgrammingLanguageShouldExistsWhenRequestMessage);
+            if (result == null) throw new BusinessException(FrameworkTechnologyMessages.FrameworkTechnologyShouldExistsWhenRequestMessage);
         }
     }
 }
