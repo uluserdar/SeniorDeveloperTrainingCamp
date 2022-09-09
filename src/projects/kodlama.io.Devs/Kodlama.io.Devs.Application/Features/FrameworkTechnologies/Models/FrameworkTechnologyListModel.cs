7fddddfb2c7f8bba4d23fd8feb5a92ebfd55f0cc
uluserdar <serdar.uludag@hotmail.com>
2022-09-09 18:41:51 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Models =>FrameworkTechnologyListModel.cs created

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Models/FrameworkTechnologyListModel.cs
@@ -0,0 +1,15 @@
+﻿using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Models
+{
+    public class FrameworkTechnologyListModel:BasePageableModel
+    {
+        public IList<FrameworkTechnologyListDto> Items { get; set; }
+    }
+}
