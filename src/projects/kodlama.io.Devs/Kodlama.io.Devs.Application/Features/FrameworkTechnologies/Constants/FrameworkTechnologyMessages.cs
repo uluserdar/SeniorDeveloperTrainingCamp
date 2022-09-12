b8c72f02c1cb72fe4d27d143864eec81ab3fe555
uluserdar <serdar.uludag@hotmail.com>
2022-09-12 09:32:07 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Rules => FrameworkTechnologyBusinessRule.cs edited
    Added ProgrammingLanguageShouldExistsWhenRequested method

--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Constants/FrameworkTechnologyMessages.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Constants/FrameworkTechnologyMessages.cs
@@ -3,7 +3,8 @@
     public static class FrameworkTechnologyMessages
     {
         public static string FrameworkTechnologyNameExistsMessage => "Framework technology name exists.";
-        public static string FrameworkTechnologyShouldExistsWhenRequestMessage => "Programming languge not exists.";
+        public static string FrameworkTechnologyShouldExistsWhenRequestMessage => "Framework technology not exists.";
 
+        public static string ProgrammingLanguageIdShouldExistsQhenRequestedMessage => "Programming languge not exists.";
     }
 }
--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Rules/FrameworkTechnologyBusinessRule.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Rules/FrameworkTechnologyBusinessRule.cs
@@ -3,16 +3,19 @@ using Core.Persistence.Paging;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Constants;
 using Kodlama.io.Devs.Application.Services.Repositories;
 using Kodlama.io.Devs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Rules
 {
     public class FrameworkTechnologyBusinessRule
     {
         private readonly IFrameworkTechnologyRepository _frameworkTechnologyRepository;
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
 
-        public FrameworkTechnologyBusinessRule(IFrameworkTechnologyRepository frameworkTechnologyRepository)
+        public FrameworkTechnologyBusinessRule(IFrameworkTechnologyRepository frameworkTechnologyRepository, IProgrammingLanguageRepository programmingLanguageRepository)
         {
             _frameworkTechnologyRepository = frameworkTechnologyRepository;
+            _programmingLanguageRepository = programmingLanguageRepository;
         }
         public async Task FrameworkTechnologyNameCanNotBeDublicatedWhenInserted(string name)
         {
@@ -30,6 +33,13 @@ namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Rules
         {
             var result = await _frameworkTechnologyRepository.GetAsync(predicate: pl => pl.Id == id,enableTracking:false);
             if (result == null) throw new BusinessException(FrameworkTechnologyMessages.FrameworkTechnologyShouldExistsWhenRequestMessage);
+        }
+
+        public async Task ProgrammingLanguageShouldExistsWhenRequested(int programmingLanguageId)
+        {
+            var result = await _programmingLanguageRepository.GetAsync(predicate: x => x.Id == programmingLanguageId);
+
+            if (result == null) throw new BusinessException(FrameworkTechnologyMessages.ProgrammingLanguageIdShouldExistsQhenRequestedMessage);
         }
     }
 }
