356e9b193c773786a9bc3182904880c4e13a183d
uluserdar <serdar.uludag@hotmail.com>
2022-09-12 14:39:49 +0300

    User,Operation Claim,UserOperationClaim,RefreshToken entities implementation and configuration for Base Dbcontext

--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.Persistence/Contexts/BaseDbContext.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Persistence/Contexts/BaseDbContext.cs
@@ -1,6 +1,8 @@
-﻿using Kodlama.io.Devs.Domain.Entities;
+﻿using Core.Security.Entities;
+using Kodlama.io.Devs.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.Versioning;
 
 namespace Kodlama.io.Devs.Persistence.Contexts
 {
@@ -9,6 +11,10 @@ namespace Kodlama.io.Devs.Persistence.Contexts
         protected IConfiguration Configuration;
         public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
         public DbSet<FrameworkTechnology> FrameworkTechnologies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -34,6 +40,54 @@ namespace Kodlama.io.Devs.Persistence.Contexts
                 ft.Property(p => p.Name).HasColumnName("Name");
                 ft.Property(p => p.ProgrammingLanguageId).HasColumnName("ProgrammingLanguageId");
                 ft.HasOne(p => p.ProgrammingLanguage);
+            });
+
+            modelBuilder.Entity<User>(user =>
+            {
+                user.ToTable("Users");
+                user.HasKey(k => k.Id);
+                user.Property(p => p.FirstName).HasColumnName("FirstName");
+                user.Property(p => p.LastName).HasColumnName("LastName");
+                user.Property(p => p.Email).HasColumnName("Email");
+                user.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+                user.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+                user.Property(p => p.Status).HasColumnName("Status");
+                user.Property(p => p.AuthenticatorType).HasColumnName("AuthenticatorType");
+                user.HasMany(user => user.UserOperationClaims);
+                user.HasMany(user => user.RefreshTokens);
+            });
+
+            modelBuilder.Entity<OperationClaim>(oc =>
+            {
+                oc.ToTable("OperationClaims");
+                oc.HasKey(k => k.Id);
+                oc.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            modelBuilder.Entity<UserOperationClaim>(uoc =>
+            {
+                uoc.ToTable("UserOperationClaims");
+                uoc.HasKey(k => k.Id);
+                uoc.Property(p => p.UserId).HasColumnName("UserId");
+                uoc.Property(p => p.OperationClaimId).HasColumnName("OperationClaimId");
+                uoc.HasOne(p => p.User);
+                uoc.HasOne(p => p.OperationClaim);
+            });
+
+            modelBuilder.Entity<RefreshToken>(rt =>
+            {
+                rt.ToTable("RefreshTokens");
+                rt.HasKey(k => k.Id);
+                rt.Property(p => p.UserId).HasColumnName("UserId");
+                rt.Property(p => p.Token).HasColumnName("Token");
+                rt.Property(p => p.Expires).HasColumnName("Expires");
+                rt.Property(p => p.Created).HasColumnName("Created");
+                rt.Property(p => p.CreatedByIp).HasColumnName("CreatedByIp");
+                rt.Property(p => p.Revoked).HasColumnName("Revoked");
+                rt.Property(p => p.RevokedByIp).HasColumnName("RevokedByIp");
+                rt.Property(p => p.ReplacedByToken).HasColumnName("ReplacedByToken");
+                rt.Property(p => p.ReasonRevoked).HasColumnName("ReasonRevoked");
+                rt.HasOne(p => p.User);
             });
 
             ProgrammingLanguage[] programmingLanguageSeeds = { new(1, "C#"), new(2, "Java"), new(3, "Pyton"), new(4, "Php") };
