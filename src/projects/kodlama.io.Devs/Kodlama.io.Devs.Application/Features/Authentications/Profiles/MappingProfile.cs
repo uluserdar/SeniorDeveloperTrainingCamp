756c0f495b66010ba73b9b6fb834e0e33d1137ba
uluserdar <serdar.uludag@hotmail.com>
2022-09-12 16:33:03 +0300

    Kodlama.io.Devs.Application\Features\Authentications\Profiles => MappingProfile added

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/Authentications/Profiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Core.Security.Dtos;
+using Core.Security.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.Authentications.Profiles
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserForRegisterDto>().ReverseMap();
+        }
+    }
+}
