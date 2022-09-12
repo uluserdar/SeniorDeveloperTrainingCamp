e92f2243ce038f324754c74b7441ce30baf65bba
uluserdar <serdar.uludag@hotmail.com>
2022-09-12 10:38:41 +0300

    Kodlama.io.Devs.Application\Features\FrameworkTechnologies\Profiles => MappingProfile.cs edited

--- src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Profiles/MappingProfile.cs
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/FrameworkTechnologies/Profiles/MappingProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.CreateFrameworkTechnology;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.DeleteFrameworkTechnology;
+using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Commands.UpdateFrameworkTechnology;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Dtos;
 using Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Models;
 using Kodlama.io.Devs.Domain.Entities;
@@ -11,8 +14,11 @@ namespace Kodlama.io.Devs.Application.Features.FrameworkTechnologies.Profiles
         public MappingProfile()
         {
             CreateMap<FrameworkTechnology, CreatedFrameworkTechnologyDto>().ReverseMap();
+            CreateMap<FrameworkTechnology, CreateFrameworkTechnologyCommand>().ReverseMap();
             CreateMap<FrameworkTechnology, UpdatedFrameworkTechnologyDto>().ReverseMap();
+            CreateMap<FrameworkTechnology, UpdateFrameworkTechnologyCommand>().ReverseMap();
             CreateMap<FrameworkTechnology, DeletedFrameworkTechnologyDto>().ReverseMap();
+            CreateMap<FrameworkTechnology, DeleteFrameworkTechnologyCommand>().ReverseMap();
             CreateMap<FrameworkTechnology,FrameworkTechnologyListDto>().ForMember(x=> x.ProgrammingLanguageName,opt=> opt.MapFrom(x=> x.ProgrammingLanguage.Name)).ForMember(x=> x.FrameworkTechnologyName,opt=> opt.MapFrom(x=> x.Name)).ReverseMap();
             CreateMap<IPaginate<FrameworkTechnology>, FrameworkTechnologyListModel>().ReverseMap();
             CreateMap<FrameworkTechnology, FrameworkTechnologyGetByIdDto>().ForMember(x => x.ProgrammingLanguageName, opt => opt.MapFrom(x => x.ProgrammingLanguage.Name)).ForMember(x => x.FrameworkTechnologyName, opt => opt.MapFrom(x => x.Name)).ReverseMap();
