46e758a77fea52d85d9b52bcfe367c490a32d994
uluserdar <serdar.uludag@hotmail.com>
2022-09-13 17:33:54 +0300

    Added Group,GroupOperationClaim and UserGroup entites Edited User entity

--- src/corePackages/Core.Security/Entities/Group.cs
+++ src/corePackages/Core.Security/Entities/Group.cs
@@ -2,13 +2,20 @@
 
 namespace Core.Security.Entities
 {
-    public class Group:Entity
+    public class Group : Entity
     {
         public string Name { get; set; }
 
-        public Group() { }
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<GroupOperationClaim> GroupOperationClaims { get; set; }   
 
-        public Group(string name):this()
+        public Group()
+        {
+            Users = new HashSet<User>();
+            GroupOperationClaims = new HashSet<GroupOperationClaim>();
+        }
+
+        public Group(string name) : this()
         {
             Name = name;
         }
---
+++ src/corePackages/Core.Security/Entities/UserGroup.cs
@@ -0,0 +1,26 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Entities
+{
+    public class UserGroup:Entity
+    {
+        public int UserId { get; set; }
+        public int GroupId { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Group Group { get; set; }
+
+        public UserGroup() { }
+
+        public UserGroup(int userId, int groupId):this()
+        {
+            UserId = userId;
+            GroupId = groupId;
+        }
+    }
+}
