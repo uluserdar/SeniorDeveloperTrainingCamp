2dd6c04d46adc5c95309da67a6a8455f77923515
uluserdar <serdar.uludag@hotmail.com>
2022-09-13 17:33:54 +0300

    Added Group,GroupOperationClaim and UserGroup entites Edited User entity

---
+++ src/corePackages/Core.Security/Entities/GroupOperationClaim.cs
@@ -0,0 +1,26 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Entities
+{
+    public class GroupOperationClaim:Entity
+    {
+        public int GroupId { get; set; }
+        public int ClaimId { get; set; }
+
+        public virtual Group Group { get; set; }
+        public virtual OperationClaim Claim { get; set; }
+
+        public GroupOperationClaim() { }
+
+        public GroupOperationClaim(int groupId, int claimId)
+        {
+            GroupId = groupId;
+            ClaimId = claimId;
+        }
+    }
+}
