200a03815861e5b80d0fea05498b3e699c328988
uluserdar <serdar.uludag@hotmail.com>
2022-09-16 16:33:17 +0300

    Added UserProfile and SocialMediaDetail repositories

---
+++ src/projects/kodlama.io.Devs/Kodlama.io.Devs.Application/Services/Repositories/ISocialMediaDetailRepository.cs
@@ -0,0 +1,7 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Services.Repositories
+{
+    public interface ISocialMediaDetailRepository : IAsyncRepository<SocialMediaDetail>, IRepository<SocialMediaDetail> { }
+}
